b91b19e454dc2eae5e99c55c0e4656d4921e250d
Nzingani <isabelnzingani@outlook.com>
2018-08-01 12:20:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P7ex4</title>
+  </head>
+  <body>
+    <form action='user.php' method='POST'>
+    <label for="lastname">Nom: </label><input type="text"name="lastname"placeholder="Nom">
+    <label for="firstname">Prénom: </label><input type="text"name="firstname"placeholder="Prénom">
+      <input type="submit"value="valider">
+    </form>
+  </body>
+</html>
